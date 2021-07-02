d01c4917b93bc6add511d3b2f77a8c3281a2770a
Zheka1988 <kveladze.kz@mail.ru>
2021-07-02 04:56:18 -0700

    User can attached file, when created a question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -43,7 +43,7 @@ class QuestionsController < ApplicationController
 
   private
   def question_params
-    params.require(:question).permit(:title, :body)
+    params.require(:question).permit(:title, :body, :file)
   end
 
   def load_question
