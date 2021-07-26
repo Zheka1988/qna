e25db39359e0a22a2e13de42dd0a50088e952c00
Zheka1988 <kveladze.kz@mail.ru>
2021-07-26 00:38:58 -0700

    added format json for CanCan::AccessDenied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
+      format.json { render nothing: true, status: :forbidden }
       format.js   { head :forbidden, alert: exception.message }
       format.html { redirect_to root_url, alert: exception.message }
     end
